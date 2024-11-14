4f4657f26d8f268d45daf98f56a60361516c5ce9
jdalrymple <jdalrymple@djgrp.com>
2024-11-14 08:11:10 -0600

    404 route, component etc (not for real 404 issues)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { GsComponent } from './gs/gs.component';
 import { HomeComponent } from './home/home.component';
 import { JsonCheckerComponent } from './json-checker/json-checker.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'gs', component: GsComponent },
   { path: 'json', component: JsonCheckerComponent },
+  { path: '404', component: NotFoundComponent },
 ];
