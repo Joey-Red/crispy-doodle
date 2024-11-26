588bcbc603a1fd5a4252f0a56795784c0e223cb8
jdalrymple <jdalrymple@djgrp.com>
2024-11-26 15:52:20 -0600

    changes

--- src/app/oh-crud/oh-crud.component.ts
+++ src/app/oh-crud/oh-crud.component.ts
@@ -1,70 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-oh-crud',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './oh-crud.component.html',
   styleUrl: './oh-crud.component.css',
 })
-export class OhCrudComponent {
-  test_users: User[] = [
-    {
-      name: 'gary',
-      img: 'https://plus.unsplash.com/premium_photo-1678197937465-bdbc4ed95815?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fHww',
-    },
-    {
-      name: 'erica',
-      img: 'https://images.unsplash.com/file-1722962837469-d5d3a3dee0c7image?w=416&dpr=2&auto=format&fit=crop&q=60',
-    },
-    {
-      name: 'angela',
-      img: 'https://images.unsplash.com/photo-1445053023192-8d45cb66099d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fHww',
-    },
-    {
-      name: 'josh',
-      img: 'https://plus.unsplash.com/premium_photo-1664536392896-cd1743f9c02c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGVyc29ufGVufDB8fDB8fHww',
-    },
-    {
-      name: 'tom',
-      img: 'https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D',
-    },
-  ];
+export class OhCrudComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  private url = 'http://localhost:999';
 
-  posts: crud[] = [
-    {
-      title: 'lorem',
-      body: 'lorem ipsum dolor sit',
-      creator: this.test_users[0],
-      post_date: DateTime.now().minus({ days: 89 }),
-    },
-    {
-      title: 'lorem ipsum',
-      body: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      creator: this.test_users[1],
-      post_date: DateTime.now().minus({ days: 37 }),
-    },
-    {
-      title: 'dolor sit',
-      body: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      creator: this.test_users[2],
-      post_date: DateTime.now().minus({ days: 7 }),
-    },
-    {
-      title: 'justo faucibus',
-      body: 'in nisi metus. Faucibus mus urna elit ante magna fames. Sagittis lectus fringilla; id fames ut mauris. Taciti at taciti placerat ridiculus tortor. Sapien dapibus vulputate vehicula et nibh penatibus; urna tempus. Senectus dictum fusce tellus justo faucibus.',
-      creator: this.test_users[3],
-      post_date: DateTime.now().minus({ days: 700 }),
-    },
-    {
-      title: 'Nec penatibus',
-      body: 'Facilisis erat primis feugiat facilisis montes felis in viverra nisi.',
-      creator: this.test_users[4],
-      post_date: DateTime.now(),
-    },
-  ];
+  ngOnInit(): void {
+    this.GetUsers();
+    this.GetPosts();
+  }
+
+  test_users: User[] = [];
+  posts: crud[] = [];
+
+  GetUsers = () => {
+    this.http.get<User[]>(`${this.url}/users`).subscribe((res) => {
+      this.test_users = res;
+    });
+  };
+
+  GetPosts = () => {
+    this.http.get<crud[]>(`${this.url}/posts`).subscribe((res) => {
+      for (let i = 0; i < res.length; i++) {
+        res[i].post_date = DateTime.fromISO(String(res[i].post_date));
+      }
+      this.posts = res;
+    });
+  };
 }
 
 interface User {
@@ -77,4 +48,7 @@ interface crud {
   body: string;
   creator: User;
   post_date: DateTime;
+  num_comments: number;
+  num_shares: number;
+  num_likes: number;
 }
